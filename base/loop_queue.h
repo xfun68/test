d0d19a9a12b0259b703aae7e20e087fcc468764d
Wang Xiaofeng <wangxiaofeng@linekong.com>
2009-06-02 19:10:42 +0800

    增加了 LoopQueueu，在封装一些操作shm的函数

---
+++ base/loop_queue.h
@@ -0,0 +1,201 @@
+#ifndef _LOOP_QUEUE_H_
+#define _LOOP_QUEUE_H_
+
+#include <inttypes.h>
+
+template <typename T>
+class LoopQueue {
+public:
+    typedef T ElemType;
+    typedef T* ElemTypePtr;
+    typedef T& ElemTypeRef;
+    typedef struct _QueueHdr_ {
+        uint32_t capacity_;
+        uint32_t elem_size_;
+        uint32_t head_;
+        uint32_t tail_;
+    } QueueHdr, *QueueHdrPtr, &QueueHdrRef;
+
+public:
+    LoopQueue(void) : hdr_(NULL), data_(NULL) {}
+
+    LoopQueue(void* const addr) {
+        setQueueAddr(addr);
+    }
+
+    ~LoopQueue(void) {
+        hdr_ = data_ = NULL;
+    }
+
+    ElemTypeRef front(void) {
+        if (NULL == hdr_) {
+            return def_msg_;
+        } else {
+            if (rsize() <= 0) {
+                return def_msg_;
+            } else {
+                return at(head());
+            }
+        }
+    }
+
+    const ElemTypeRef front(void) const {
+        return front();
+    }
+
+    ElemTypeRef back(void) {
+        if (NULL == hdr_) {
+            return def_msg_;
+        } else {
+            if (rsize() <= 0) {
+                return def_msg_;
+            } else {
+                return at(tail() - 1);
+            }
+        }
+    }
+
+    const ElemTypeRef back(void) const {
+        return back();
+    }
+
+    ElemTypeRef end(void) {
+        if (NULL == hdr_) {
+            return def_msg_;
+        } else {
+            if ((rsize() <= 0) || (rsize() >= capacity())) {
+                return front();
+            } else {
+                return at(hdr_->tail_);
+            }
+        }
+    }
+
+    const ElemTypeRef end(void) const {
+        return end();
+    }
+    ElemTypeRef at(uint32_t pos) {
+        if (NULL == hdr_) {
+            return def_msg_;
+        } else {
+            return data_[pos % capacity()];
+        }
+    }
+
+    const ElemTypeRef at(uint32_t pos) const {
+        return at(pos);
+    }
+
+    int32_t pop(void) {
+        if (NULL == hdr_) {
+            return -1;
+        } else {
+            if (rsize() <= 0) {
+                return -1;
+            } else {
+                hdr_->head_ = ++head();
+                return 0;
+            }
+        }
+    }
+
+    int32_t push(const ElemTypeRef val) {
+        if (NULL == hdr_) {
+            return -1;
+        } else {
+            if (&val != &(end())) {
+                end() = val;
+            }
+            hdr_->tail_ = ++tail();
+            return 0;
+        }
+    }
+
+    int32_t clear(void) {
+        if (NULL == hdr_) {
+            return -1;
+        } else {
+            return hdr_->head_ = hdr_->tail_ = 0;
+        }
+    }
+
+    bool empty(void) const {
+        return size() == 0;
+    }
+
+    uint32_t capacity(void) const {
+        if (NULL == hdr_) {
+            return 0;
+        } else {
+            return hdr_->capacity_;
+        }
+    }
+
+    uint32_t size(void) const {
+        return rsize() > capacity() ? capacity() : rsize();
+    }
+
+    uint32_t rsize(void) const {
+        if (NULL == hdr_) {
+            return 0;
+        } else {
+            return hdr_->tail_ - hdr_->head;
+        }
+    }
+
+    uint32_t head(void) const {
+        if (NULL == hdr_) {
+            return -1;
+        } else {
+            if (rsize() > capacity()) {
+                return hdr_->tail_ - capacity();
+            } else {
+                return hdr_->head_;
+            }
+        }
+    }
+
+    uint32_t tail(void) const {
+        if (NULL == hdr_) {
+            return -1;
+        } else {
+            if (rsize() < 0) {
+                return hdr_->head;
+            } else {
+                return hdr_->tail_;
+            }
+        }
+    }
+
+    int32_t setQueueAddr(void* const addr) {
+        if (NULL == addr) {
+            hdr_ = data_ = NULL;
+        } else {
+            hdr_ = (QueueHdrPtr)addr;
+            data_ = (ElemTypePtr)((int8_t*)addr + sizeof(QueueHdr));
+        }
+        return 0;
+    }
+
+    int32_t setQueueAddr(void* const addr, uint32_t capacity, size_t elem_size) {
+        if (NULL == addr) {
+            return -1;
+        } else {
+            setQueueAddr(addr);
+            hdr_->capacity_ = capacity;
+            hdr_->elem_size_ = elem_size;
+            return 0;
+        }
+    }
+
+private:
+    LoopQueue(const LoopQueue& rhs);
+    LoopQueue& operator = (const LoopQueue& rhs);
+
+    QueueHdrPtr hdr_;
+    ElemTypePtr data_;
+    ElemType def_msg_;
+};
+
+#endif //  _LOOP_QUEUE_H_
+
---
+++ base/shm.h
@@ -0,0 +1,21 @@
+#ifndef _SHM_H_
+#define _SHM_H_
+
+#include <inttypes.h>
+/*{{{*/
+/*
+class SHM {
+public:
+    SHM(void);
+    ~SHM(void);
+
+    int32_t initialize(uint32_t appID);
+    int32_t release(void);
+
+private:
+    SHM(const SHM& rhs);
+    SHM& operator = (const SHM& rhs);
+}; */ /*}}}*/
+
+#endif //  _SHM_H_
+
